2fe1256e990056a91fa75dfbcc443fe25673d7fb
khanian <khanylee@gmail.com>
2023-11-18 23:11:43 +0900

    user list to story area.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,8 +5,65 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Icon(Icons.home, size: 100),
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          StroyArea(),
+          FeedList(),
+        ],
+      ),
     );
+  }
+}
+
+class StroyArea extends StatelessWidget {
+  const StroyArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(10, (index) => UserStory(userName: 'user $index')),
+      ),
+    );
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+  const UserStory({
+    required this.userName,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+              color: Colors.blue.shade300,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+          Text(userName),
+        ],
+      ),
+    );
+  }
+}
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
